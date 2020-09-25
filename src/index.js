0fda06f64c99c2b8b45dae46dbbfe528e2d1806a
Nozomi <13172293+TheNozomi@users.noreply.github.com>
2020-09-25 02:39:10 -0300

    Backend URL fallback

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
 import VueFeathers from './plugins/feathers.js';
 
-const socket = io(import.meta.env.VITE_YVES_API);
+const socket = io(import.meta.env.VITE_YVES_API || 'http://localhost:3030');
 const client = feathers();
 client.configure(socketio(socket));
 app.use(VueFeathers, {client: client});
